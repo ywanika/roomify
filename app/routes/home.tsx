3ab3c26c768c26fb0c042a7d39cc90688d49e6ae
Anika Sharma <34630890+ywanika@users.noreply.github.com>
2026-06-02 17:37:23 -0700

    homepage

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,7 @@
 import type { Route } from "./+types/home";
 import Navbar from "../../components/Navbar";
+import {ArrowRight, Layers} from "lucide-react";
+import {Button} from "../../components/ui/Button";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -11,8 +13,65 @@ export function meta({}: Route.MetaArgs) {
 export default function Home() {
   return (
       <div className="home">
-        <h1 className="text-3xl text-indigo-700 font-extrabold">Home</h1>
-        <Navbar />
+          <Navbar />
+          <section className="hero">
+              <div className="announce">
+                  <div className="dot">
+                      <div className="pulse"></div>
+                  </div>
+
+                  <p>Introducing Roomify 2.0</p>
+              </div>
+
+              <h1>Build beautiful spaces at the speed of thought with Roomify</h1>
+
+              <p className="subtitle">Roomify is an AI-first design environment that helps you visualize, render, and ship
+              architectural projects faster than ever.</p>
+
+              <div className="actions">
+                  <a href="#upload" className="cta">Start Building <ArrowRight className="icon" />
+                  </a>
+
+                  <Button variant="outline" size="lg" className="demo">Watch Demo</Button>
+              </div>
+
+              <div id="upload" className="upload-shell">
+                  <div className="grid-overlay"/>
+
+                  <div className="upload-card">
+                      <div className="upload-head">
+                          <div className="upload-icon">
+                              <Layers className="icon"/>
+                          </div>
+                          <h3>Upload your floor plan</h3>
+                          <p>Support JPG, PNG formats up to 10MB</p>
+                      </div>
+                      <p>Upload images</p>
+                  </div>
+              </div>
+          </section>
+          <section className="projects">
+              <div className="section-inner">
+                  <div className="section-head">
+                      <div className="copy">
+                          <h2>Projects</h2>
+                          <p> Your latest work and shared community projects, all in one place.</p>
+                      </div>
+                  </div>
+                  <div className="projects-grid">
+                      <div className="project-card group">
+                          <div className="preview">
+                              <img src="https://roomify-mlhuk267-dfwu1i.puter.site/projects/1770803585402/rendered.png"
+                              alt="Project Preview"/>
+                              <div className="badge">
+                                  <span>Community</span>
+                              </div>
+                          </div>
+
+                      </div>
+                  </div>
+              </div>
+          </section>
       </div>
   );
 }
